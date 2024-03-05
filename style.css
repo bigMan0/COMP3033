3915b0ea15a9bb947888be392f40971f49b12e0d
Cameron <you@example.com>
2024-03-05 15:53:21 -0400

    Changes

--- style.css
+++ style.css
@@ -1,3 +1,3 @@
 /* COMP 3033 Final  Project: Calendar Web Application
-   Authors: Cameron Burgoyne () and Kylie DeViller (162298d)
+   Authors: Cameron Burgoyne (162210b) and Kylie DeViller (162298d)
    Date: April 5th 2024 */
